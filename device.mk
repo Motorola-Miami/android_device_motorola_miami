56b6461b4c4026254636661927b3eb88cccd5f83
Rakhshan Ali <alirakhshan7070@gmail.com>
2024-12-29 22:24:34 +0530

    miami: build dolby atmos from hardware/motorola
    * Reference: https://github.com/userariii/vendor_motorola-dolby.git

--- device.mk
+++ device.mk
@@ -39,6 +39,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/display_id_4630947043778501761.xml:$(TARGET_COPY_OUT_VENDOR)/etc/displayconfig/display_id_4630947043778501761.xml
 
+# Dolby
+$(call inherit-product, hardware/motorola/dolby/setup.mk)
+
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.3-service.miami

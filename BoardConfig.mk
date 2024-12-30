c796ecd709813a2d24495a5587c339f250f5a796
Bashid <121305132+Rakhshan7070@users.noreply.github.com>
2024-12-30 19:09:18 +0530

    miami: Forcibly include common trees
    * No hopes of booting without this.
    
    Co-Authored-By: Nolen Johnson <johnsonnolen@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,7 +5,7 @@
 #
 
 # Inherit from motorola sm6375-common
--include device/motorola/sm6375-common/BoardConfigCommon.mk
+include device/motorola/sm6375-common/BoardConfigCommon.mk
 
 DEVICE_PATH := device/motorola/miami
 
@@ -47,5 +47,5 @@ TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 VENDOR_SECURITY_PATCH := 2024-05-01
 
 # Inherit from the proprietary version
--include vendor/motorola/miami/BoardConfigVendor.mk
+include vendor/motorola/miami/BoardConfigVendor.mk
 -include hardware/motorola/dolby/BoardConfigVendor.mk

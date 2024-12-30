4af63f5c1f8886dbf82a13b26e7521a12f26182b
Bashid <121305132+Rakhshan7070@users.noreply.github.com>
2024-12-30 19:18:14 +0530

    miami: Rename defconfig to miami_defconfig

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -21,8 +21,8 @@ SOONG_CONFIG_qtidisplay_udfps := true
 
 # Kernel
 BOARD_KERNEL_CMDLINE += androidboot.hab.product=miami
-TARGET_KERNEL_CONFIG := vendor/aicp_miami_defconfig
 TARGET_KERNEL_SOURCE := kernel/motorola/miami
+TARGET_KERNEL_CONFIG := vendor/miami_defconfig
 
 # Kernel Modules
 BOARD_VENDOR_KERNEL_MODULES_LOAD := $(strip $(shell cat $(DEVICE_PATH)/modules.load))

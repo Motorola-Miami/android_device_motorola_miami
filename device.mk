90c5c5ab1e81c1f2107b7bb1e371aaab9edf5fc8
Marc Bourgoin <themard85@gmail.com>
2025-03-31 23:02:16 -0600

    fixup! miami: Debrand manifests & overlays
    Change-Id: Iaa0283309aec34666cc9a217b0b25fd47007fe32

--- device.mk
+++ device.mk
@@ -25,11 +25,11 @@ $(call inherit-product, device/motorola/sm6375-common/common.mk)
 
 # Overlay
 PRODUCT_PACKAGES += \
-    FrameworksResMiami \
-    LineageSystemUIMiami \
-    SettingsResMiami \
-    SystemUIResMiami \
-    WifiResMiami
+    FrameworksResDevice \
+    LineageSystemUIDevice \
+    SettingsResDevice \
+    SystemUIResDevice \
+    WifiResDevice
 
 # Display
 PRODUCT_COPY_FILES += \

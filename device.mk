ed80731cfb8369e953ddf3c6305631c512ca035e
Rakhshan Ali <alirakhshan7070@gmail.com>
2025-04-09 00:26:06 +0530

    miami: move audio configs to common tree

--- device.mk
+++ device.mk
@@ -31,11 +31,6 @@ PRODUCT_PACKAGES += \
     SystemUIResMiami \
     WifiResMiami
 
-# Audio
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
-    $(LOCAL_PATH)/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
-
 # Dolby
 $(call inherit-product, hardware/motorola/dolby/setup.mk)
 
